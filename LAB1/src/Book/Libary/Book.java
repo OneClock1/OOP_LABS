2edfd424bda2b51ffe3f98c26f89e95e4f4cf926
OneClock1 <toly1539@gmail.com>
2023-03-06 21:01:35 +0200

    Update LAB1

--- LAB1/src/Book/Libary/Book.java
+++ LAB1/src/Book/Libary/Book.java
@@ -15,8 +15,7 @@ public class Book {
     public Book()
     {
     }
-    public Book(int id, String name, String author, String publication, int yearPublication, int numberPages, float price)
-    {
+    public Book(int id, String name, String author, String publication, int yearPublication, int numberPages, float price) {
         this.id = id;
         this.name = name;
         this.author = author;
@@ -27,26 +26,41 @@ public class Book {
     }
 
 
-    public int getId() {return id;}
-    public void setId(int id) {this.id = id;}
+    public int getId(){
+        return id;}
+    public void setId(int id){
+        this.id = id;}
 
-    public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
+    public String getName(){
+        return name;}
+    public void setName(String name){
+        this.name = name;}
 
-    public String getAuthor() {return author;}
-    public void setAuthor(String author) {this.author = author;}
+    public String getAuthor(){
+        return author;}
+    public void setAuthor(String author){
+        this.author = author;}
 
-    public String getPublication() {return publication;}
-    public void setPublication(String publication) {this.publication = publication;}
+    public String getPublication(){
+        return publication;}
+    public void setPublication(String publication){
+        this.publication = publication;}
 
-    public int getYearPublication() {return yearPublication;}
-    public void setYearPublication(int yearPublication) {this.yearPublication = yearPublication;}
+    public int getYearPublication(){
+        return yearPublication;}
+    public void setYearPublication(int yearPublication){
+        this.yearPublication = yearPublication;}
 
-    public int getNumberPages() {return numberPages;}
-    public void setNumberPages(int numberPages) {this.numberPages = numberPages;}
+    public int getNumberPages(){
+        return numberPages;}
+    public void setNumberPages(int numberPages){
+        this.numberPages = numberPages;}
 
-    public float getPrice() {return price;}
-    public void setPrice(float price) {this.price = price;}
+    public float getPrice(){
+        return price;}
+    public void setPrice(float price){
+        this.price = price;}
+
     @Override
     public String toString(){
         return "Книжка:"+
--- LAB1/src/Main.java
+++ LAB1/src/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
+
 public class Main {
     private ArrayList<Book> bookList = new ArrayList<Book>();
     public static void main(String[] args) {
